eb2440e7a087f4efc5b4b25143b65ab31e8f2818
aldrix-max <av0275502@gmail.com>
2025-07-30 23:15:07 +0200

    deployment

--- Flet_App/main.py
+++ Flet_App/main.py
@@ -19,4 +19,4 @@ def main(page: ft.Page):
     page.on_route_change = route_change
     page.go(page.route)
 
-ft.app(target=main, assets_dir="assets")
+ft.app(target=main, assets_dir="assets", port=8080, view=ft.WEB_BROWSER)
